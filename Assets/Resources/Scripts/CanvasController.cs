d2d6ac4519f0aa5f2a20bc23205405c43af9f6a3
jamesrosstwo <james.rosstwo@gmail.com>
2019-02-09 15:33:30 -0500

    speed up readings

--- Assets/Resources/Scripts/CanvasController.cs
+++ Assets/Resources/Scripts/CanvasController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CanvasController : MonoBehaviour
 {
-	private float refreshCooldown = 0.1f;
+	private float refreshCooldown = 0.06f;
 	private float _refreshTimer;
 	
 	void Start()
@@ -12,9 +12,5 @@ public class CanvasController : MonoBehaviour
 		Quadcopter.SetupSocket();
 		_refreshTimer = refreshCooldown;
 		StartCoroutine(Quadcopter.PullReadings(refreshCooldown));
-	}
-	
-	void Update ()
-	{
 	}
 }
